0c24d33536489993279e7067c9fe18f06fdb25fb
MuhammadZubair <mzhassan444@gmail.com>
2020-06-10 01:11:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>New App</h1>
+          Edit <code>src/App.js</code> and save to reload.
         <a
           className="App-link"
           href="https://reactjs.org"
