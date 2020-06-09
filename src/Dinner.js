214ce5627a88e3b6e8c8fe8f15e3a8988de82bf8
MuhammadZubair <mzhassan444@gmail.com>
2020-06-10 01:28:40 +0500

    Add new Dinner.js File"

---
+++ src/Dinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Dinner(props) {
+  return (
+      <div>
+      <h1>Today we are serving {props.dishName}</h1>
+      
+    </div>
+  )
+}
+
+export default Dinner;
