de74ab9f1fdbbdb23d096246c7b8cffed571f981
MuhammadZubair <mzhassan444@gmail.com>
2020-06-10 11:37:01 +0500

    Update Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -4,7 +4,8 @@ import React from 'react';
 function Dinner(props) {
   return (
       <div>
-      <h1>Today we are serving {props.dishName}</h1>
+      {/* <h1>Today we are serving {props.dishName}</h1> */}
+      <input type="button" value="Submit"></input>
       
     </div>
   )
